895607dc90a2870af5db06741903665944c66180
Joel Pfannschmidt <joel.pfannschmidt@opal-rt.com>
2023-04-28 22:21:13 -0700

    Added return_nearest_neighbor function

--- QuadTree.py
+++ QuadTree.py
@@ -1,29 +1,43 @@
-
+import math
 
 class Boundary():
-    def __init__(self, x, y, w, h):
+    def __init__(self, x, y, w, h=0, type='rect'):
         self.x = x
         self.y = y
-        self.w = w
-        self.h = h
+        self.type = type
 
-        if self.w < 0:
-            self.x = x + w
-            self.w = abs(w)
-        
-        if self.h < 0:
-            self.y = y + h
-            self.h = abs(h)
-        
+        if self.type == 'rect':
+            self.w = w
+            self.h = h
+
+            if self.w < 0:
+                self.x = x + w
+                self.w = abs(w)
+            
+            if self.h < 0:
+                self.y = y + h
+                self.h = abs(h)
+
+        elif self.type =='circle':
+            self.r = abs(w)
+
+
+
+class Entity():
+    def __init__(self, x, y, entity=None):
+        self.x = x
+        self.y = y
+        self.entity = entity
+
 
 
 class QuadTree():
     def __init__(self, boundary, capacity, level):
         self.boundary = boundary
-        self.points = []
+        self.entities = []
         self.capacity = capacity
         self.divided = False
-        self.num_points = 0
+        self.num_entities = 0
         self.level = level
 
     def subdivide(self):
@@ -40,148 +54,94 @@ class QuadTree():
         self.southwest = QuadTree(boundary, self.capacity, self.level+1)
 
 
-    def within_bounds(self, boundary, point):
-        if (point[0] >= boundary.x and point[0] <= boundary.x + boundary.w) and (point[1] >= boundary.y and point[1] <= boundary.y + boundary.h):
-            return True
+    def within_bounds(self, boundary, entity):
+        if boundary.type == 'rect':
+            if (entity.x >= boundary.x and entity.x <= boundary.x + boundary.w) and (entity.y >= boundary.y and entity.y <= boundary.y + boundary.h):
+                return True
+        
+        if boundary.type == 'circle':
+            if (math.sqrt((entity.x - boundary.x) ** 2 + (entity.y - boundary.y) ** 2) <= boundary.r):
+                return True
         
         return False
+    
 
 
+    def insert_entity(self, entity):
 
-    def insert_point(self, point):
-
-        if self.within_bounds(self.boundary, point):
-            self.num_points += 1
+        if self.within_bounds(self.boundary, entity):
+            self.num_entities += 1
         else:
             return
 
-        if len(self.points) < self.capacity:
-            self.points.append(point)
+        if len(self.entities) < self.capacity:
+            self.entities.append(entity)
         else: # If over capacity, sub-divide
             if not self.divided:
                 self.subdivide()
                 self.divided = True
             
-            self.northeast.insert_point(point)
-            self.northwest.insert_point(point)
-            self.southeast.insert_point(point)
-            self.southwest.insert_point(point)
+            self.northeast.insert_entity(entity)
+            self.northwest.insert_entity(entity)
+            self.southeast.insert_entity(entity)
+            self.southwest.insert_entity(entity)
 
-    def return_contained_points(self, search_boundary, contained_points):
-        def check_range(quadtree, search_boundary, contained_points):
-            for i in range(0, len(quadtree.points)):
-                if quadtree.within_bounds(search_boundary, quadtree.points[i]):
-                    contained_points.append(quadtree.points[i])
-            return contained_points
+    def return_contained_entities(self, search_boundary, contained_entities):
+        def check_range(quadtree, search_boundary, contained_entities):
+            for i in range(0, len(quadtree.entities)):
+                if quadtree.within_bounds(search_boundary, quadtree.entities[i]):
+                    contained_entities.append(quadtree.entities[i])
+            return contained_entities
 
-        contained_points = check_range(self, search_boundary, contained_points)
+        contained_entities = check_range(self, search_boundary, contained_entities)
 
         if self.divided:
-            contained_points = self.northeast.return_contained_points(search_boundary, contained_points)
-            contained_points = self.northwest.return_contained_points(search_boundary, contained_points)
-            contained_points = self.southeast.return_contained_points(search_boundary, contained_points)
-            contained_points = self.southwest.return_contained_points(search_boundary, contained_points)
+            contained_entities = self.northeast.return_contained_entities(search_boundary, contained_entities)
+            contained_entities = self.northwest.return_contained_entities(search_boundary, contained_entities)
+            contained_entities = self.southeast.return_contained_entities(search_boundary, contained_entities)
+            contained_entities = self.southwest.return_contained_entities(search_boundary, contained_entities)
 
-        return contained_points
-
-
+        return contained_entities
 
 
 
+    def return_nearest_neighbor(self, entity, search_boundary, entity_list):
 
+        if search_boundary != None:
+            pass
+            #entity_list = self.return_contained_entities(search_boundary, contained_entities=[])
+
+
+        def calc_distance(entity, pos, min_distance, entity_found):
+            distance = math.sqrt((entity.x - pos[0]) ** 2 + (entity.y - pos[1]) ** 2)
+            #distance = (entity.x - pos[0]) + (entity.y - pos[1])
+            if distance < min_distance and distance > 0:
+                entity_found = entity
+                min_distance = distance                
+            return entity_found, min_distance
+        
+
+        min_distance = float('inf')
+        entity_found = None
+        pos = [entity.x, entity.y]
+        for entity in entity_list:
+            entity_found, min_distance = calc_distance(entity, pos, min_distance, entity_found)
+
+        return entity_found, min_distance
+
+
+    def get_all_entities(self, entity_list):
+        for entity in self.entities:            
+            entity_list.append(entity)
+
+        if self.divided:
+            entity_list = self.northeast.get_all_entities(entity_list)
+            entity_list = self.northwest.get_all_entities(entity_list)
+            entity_list = self.southeast.get_all_entities(entity_list)
+            entity_list = self.southwest.get_all_entities(entity_list)
+
+        return entity_list
     
-def show(quadtree, surface, search_boundary, contained_points, pygame):
-
-    # If top-level of tree
-    if quadtree.level == 0:
-        contained_points = quadtree.return_contained_points(search_boundary, [])
-        pygame.Surface.fill(surface, [0,0,0])
-    
-
-    # Draw boundary of tree node
-    rect_dim = [quadtree.boundary.x, quadtree.boundary.y, quadtree.boundary.w, quadtree.boundary.h]
-    pygame.draw.rect(surface, [255, 0, 0], pygame.Rect(rect_dim), width=1)
-
-
-    # Draw points in different colors
-    for i in range(0, len(quadtree.points)):
-        if quadtree.divided:
-            color = [0, 255, 0]
-        else:
-            color = [0, 0, 255]
-
-        if quadtree.points[i] in contained_points:
-            color = [255, 255, 255]
-
-        pygame.draw.circle(surface, color, quadtree.points[i], 2)
-
-    # Perform recursion if tree node has child nodes
-    if quadtree.divided:
-        show(quadtree.northeast, surface, search_boundary, contained_points, pygame)
-        show(quadtree.northwest, surface, search_boundary, contained_points, pygame)
-        show(quadtree.southeast, surface, search_boundary, contained_points, pygame)
-        show(quadtree.southwest, surface, search_boundary, contained_points, pygame)
-
-    if quadtree.level == 0:
-        print(search_boundary.x, search_boundary.y, search_boundary.w, search_boundary.h)
-        pygame.draw.rect(surface, [0, 255, 255], pygame.Rect(search_boundary.x, search_boundary.y, search_boundary.w, search_boundary.h), width=1)
-    
-                
-
-
-
-
-def quadtree_test():
-    WIDTH = 1200
-    HEIGHT = 600
-    boundary = Boundary(0, 0, WIDTH, HEIGHT)
-
-
-    import pygame, sys
-    pygame.init()
-    surface = pygame.display.set_mode((WIDTH,HEIGHT))
-    search_boundary = Boundary(-10, -10, 0, 0)
-    myTree = QuadTree(boundary, 1, 0)
-    redraw = True
-    contained_points = []
-    fps = 60
-    clock = pygame.time.Clock()
-    init_pos = [0, 0]
-
     
 
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-
-            pos = pygame.mouse.get_pos()
-
-            if pygame.mouse.get_pressed()[0]:
-                myTree.insert_point(pos)
-                redraw = True
-
-            if pygame.mouse.get_pressed()[1]:
-                search_boundary = Boundary(init_pos[0], init_pos[1], pos[0]-init_pos[0], pos[1]-init_pos[1])
-                redraw = True
-
-
-            if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[1]:
-                init_pos = pos
-                search_boundary = Boundary(init_pos[0], init_pos[1], 0, 0)
-
-
-            if redraw:
-                show(myTree, surface, search_boundary, contained_points, pygame)  
-                pygame.display.flip()
-                redraw = False              
-                
-
-        clock.tick(fps)
-
-
-if __name__ == "__main__":
-
-    quadtree_test()
+    
